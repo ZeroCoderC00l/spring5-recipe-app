c111e8190d86d748a45f568de27f97d355cc985d
ZeroCoderC00l <luis.a.santana.sap@gmail.com>
2019-07-15 18:52:04 -0400

    Implementing JUNIT and Mockito.

---
+++ src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -0,0 +1,43 @@
+package guru.springframework.controllers;
+
+import guru.springframework.repositories.RecipeRepository;
+import guru.springframework.services.RecipeService;
+import guru.springframework.services.RecipeServiceImpl;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.anySet;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+public class IndexControllerTest {
+
+    @Mock
+    RecipeService recipeService;
+
+    @Mock
+    Model model;
+
+    IndexController indexController;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        indexController = new IndexController(recipeService);
+    }
+
+    @Test
+    public void getIndexPage() {
+        String viewname = indexController.getIndexPage(model);
+
+        assertEquals("index", viewname);
+        verify(recipeService, times(1)).getRecipes();
+        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+    }
+
+}
